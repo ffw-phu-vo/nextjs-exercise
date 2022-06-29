40d7166c156685f22ef55a31525be837555a7c4a
Phu Vo <phu.vo@ffwagency.com>
2022-06-29 10:05:29 +0700

    Login page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,13 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    borderWidth: {
+      DEFAULT: '1px',
+      '0': '0',
+      '2': '2px',
+      '3': '3px',
+      '4': '4px',
+    },
     extend: {},
   },
   plugins: [],
